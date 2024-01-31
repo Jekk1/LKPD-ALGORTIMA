85bef193386c15f7e56939b9ac210fc944a7326e
Jekk1 <dzakwan.d.pratama@gmail.com>
2024-01-31 14:14:06 +0700

    LKPD ALGORTIMA 2-5

---
+++ LKPD 2/algortima2.js
@@ -0,0 +1,54 @@
+// NOMOR 1
+
+// let karyawan = "doyak";
+
+// let gajipokok = 1000000;
+
+// let tunjangan = gajipokok * 0.2; 
+
+// console.log(tunjangan)
+
+// let pajak = gajipokok * 0.15;
+// console.log(pajak)
+
+// let gajibersih = gajipokok + tunjangan - pajak;
+// console.log(gajibersih)
+
+
+// // NOMOR 2
+
+// let j = 1;
+// let m = 30;
+// let d = 40;
+
+// let jked = 3600 * j;
+// let mked = 60 * m;
+// let dked = 1 * d;
+
+// let totaldetik = (jked + mked + dked);
+// console.log(totaldetik)
+
+
+// let totaldetik = (j * 3600) + (m * 60) + d;
+// console.log(totaldetik)
+
+
+// nomor 3 
+
+// let  banyakdetik = 5440;
+
+// let jam = Math.floor( banyakdetik / 3600);
+// let sisabagi = banyakdetik % 3600;
+// let menit = Math.floor( sisabagi / 60);
+// let detik = sisabagi %  60;
+
+// console.log(jam + "jam" + menit + " menit" + detik + "detik")
+ 
+
+
+
+
+
+
+
+
